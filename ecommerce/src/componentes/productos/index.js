2f2c2d0427277c4128f50ca9917455a0d6815c1e
Jcastaho <juan.castano957@pascualbravo.edu.co>
2023-11-04 22:09:47 -0500

    inicio
    Inicio con varios productos

--- ecommerce/src/componentes/productos/index.js
+++ ecommerce/src/componentes/productos/index.js
@@ -1,9 +1,92 @@
 import React from "react";
+import IMG  from "../../images/img01.jpg"
 
 export const ProductosLista = () => {
     return(
-        <div>
-            <h1>Productos Lista</h1>
+        <>
+        <h1 className="title">PRODUCTOS</h1>
+        <div className="productos">
+            <div className="producto">
+                <a href="#">
+                    <div className="producto__img">
+                        <img src={ IMG } alt="" />
+                    </div>
+                </a>
+                <div className="producto__footer">
+                    <hi> Title </hi>
+                    <p> Categoria </p>
+                    <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">
+                        Añadir al carrito
+                    </button>
+                    <div>
+                        <a href="#" className="btn">Vista</a>
+                    </div>
+                </div>
+            </div>
+            <div className="producto">
+                <a href="#">
+                    <div className="producto__img">
+                        <img src={IMG} alt="" />
+                    </div>
+                </a>
+                <div className="producto__footer">
+                    <hi> Title </hi>
+                    <p> Categoria </p>
+                    <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">
+                        Añadir al carrito
+                    </button>
+                    <div>
+                        <a href="#" className="btn">Vista</a>
+                    </div>
+                </div>
+            </div>
+            <div className="producto">
+                <a href="#">
+                    <div className="producto__img">
+                        <img src={IMG} alt="" />
+                    </div>
+                </a>
+                <div className="producto__footer">
+                    <hi> Title </hi>
+                    <p> Categoria </p>
+                    <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">
+                        Añadir al carrito
+                    </button>
+                    <div>
+                        <a href="#" className="btn">Vista</a>
+                    </div>
+                </div>
+            </div>
+            <div className="producto">
+                <a href="#">
+                    <div className="producto__img">
+                        <img src={IMG} alt="" />
+                    </div>
+                </a>
+                <div className="producto__footer">
+                    <hi> Title </hi>
+                    <p> Categoria </p>
+                    <p className="price">$320</p>
+                </div>
+                <div className="buttom">
+                    <button className="btn">
+                        Añadir al carrito
+                    </button>
+                    <div>
+                        <a href="#" className="btn">Vista</a>
+                    </div>
+                </div>
+            </div>
         </div>
+    </>
     )
 }
