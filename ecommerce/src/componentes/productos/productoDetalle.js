b5122f919f55201fe45f54465f4a476f3f4ccc96
Juangui28 <jugulome28@gmail.com>
2023-11-09 15:43:35 -0500

    Terminada la sección de visualizar el producto
    El producto se puede visualizar y ver su información, se puede agregar al carrito desde esa ventana y podemos observar productos relacionados.

--- ecommerce/src/componentes/productos/productoDetalle.js
+++ ecommerce/src/componentes/productos/productoDetalle.js
@@ -1,22 +1,27 @@
 import React, { useContext, useState, useEffect } from 'react';
 import {DataContext} from "../../context/dataprovider";
 import {useParams} from "react-router-dom";
-
+import { ProductoItem } from './productoitem';
 
 export const ProductoDetalle = () => {
 
     const value = useContext(DataContext)
     const [productos] = value.productos;
+    const addCarrito = value.addCarrito;
     const [detalle, setDetalle] = useState([]);
     const params = useParams();
+    let item = 0;
     
     useEffect(() =>{
         productos.forEach(producto => {
+            item = 0;
             if(producto.id === parseInt(params.id)){
                 setDetalle(producto)
+
             }
         });
     },[params.id, productos])
+
 
   return (
     <>
@@ -41,9 +46,8 @@ export const ProductoDetalle = () => {
                         <p>Tamaño</p>
                     </div>
                 </div>
-                <button>Añadir al carrito</button>
-                <img src="https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-Black-Nylon/Images/Nike-LD-Waffle-Sacai-Black-Nylon/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000" alt={detalle.title}/>
-                <input type='range' min="1" max="36"/>
+                <button onClick={()=> addCarrito(detalle.id)}>Añadir al carrito</button>
+                <img src={detalle.image} alt={detalle.title}/>
                 <div className='description'>
                     <p><b>Descripcion:</b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
@@ -52,9 +56,28 @@ export const ProductoDetalle = () => {
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <br/><br/><br/><br/>
                 </div>
             </div>
         }
+
+        <h2 className='relacionado'>Productos Relacionados</h2>
+        <div className="productos">
+            {productos.map((producto) => {
+                if((item < 6)&&(detalle.category === producto.category)){
+                    item++;
+                    return <ProductoItem 
+                    key={producto.id}
+                    id={producto.id}
+                    title={producto.title}
+                    price={producto.price}
+                    image={producto.image}
+                    category={producto.category}
+                    cantidad={producto.cantidad}/>
+                }
+            })
+            }                
+        </div>
     </>
   )
 }
