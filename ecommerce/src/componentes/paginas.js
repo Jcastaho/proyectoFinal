30eef2bbe2d057e0b9b0a517b8786dd0ce4a90e9
Juangui28 <jugulome28@gmail.com>
2023-11-08 22:28:10 -0500

    Avance de la vista del producto
    Se ha iniciado el diseño de la pestaña "Detalle productos"

--- ecommerce/src/componentes/paginas.js
+++ ecommerce/src/componentes/paginas.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { ProductosLista } from "./productos"; 
 import { Inicio } from "./inicio";
 import { Login } from "./login";
+import {ProductoDetalle} from './productos/productoDetalle'
 
 export const Paginas = () => {
     return(
@@ -12,6 +13,7 @@ export const Paginas = () => {
                 <Route path="/login" element={<Login/>} />
                 <Route path="/inicio" element={<Inicio/>} />
                 <Route path="/productos" element={<ProductosLista/>} />
+                <Route path="/producto/:id" element={<ProductoDetalle/>} />
             </Routes>
         </section>
     )
--- ecommerce/src/componentes/productos/productoDetalle.js
+++ ecommerce/src/componentes/productos/productoDetalle.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import {DataContext} from "../../context/dataprovider";
+import {useParams} from "react-router-dom";
 
-export const productoDetalle = () => {
+
+export const ProductoDetalle = () => {
+
+    const value = useContext(DataContext)
+    const [productos] = value.productos;
+    const [detalle, setDetalle] = useState([]);
+    const params = useParams();
+    
+    useEffect(() =>{
+        productos.forEach(producto => {
+            if(producto.id === parseInt(params.id)){
+                setDetalle(producto)
+            }
+        });
+    },[params.id, productos])
+
   return (
-    <div>
-        <h1>Detalles del producto</h1>
-    </div>
+    <>
+        {
+            <div className='detalles'>
+                <h2>{detalle.title}</h2>
+                <p className='price'>${detalle.price}</p>
+                <div className='grid'>
+                    <p className='nuevo'>Nuevo</p>
+                    <div className='size'>
+                        <select placeholder='Tamaño'>
+                            <option value="1">1</option>
+                            <option value="1">2</option>
+                            <option value="1">3</option>
+                            <option value="1">4</option>
+                            <option value="1">5</option>
+                            <option value="1">6</option>
+                            <option value="1">7</option>
+                            <option value="1">8</option>
+                            <option value="1">9</option>
+                        </select>
+                        <p>Tamaño</p>
+                    </div>
+                </div>
+                <button>Añadir al carrito</button>
+                <img src="https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-Black-Nylon/Images/Nike-LD-Waffle-Sacai-Black-Nylon/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000" alt={detalle.title}/>
+                <input type='range' min="1" max="36"/>
+                <div className='description'>
+                    <p><b>Descripcion:</b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+        }
+    </>
   )
 }
